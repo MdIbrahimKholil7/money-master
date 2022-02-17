55e73f23a59dfda4218c25df1c45ec7b5b5b0d59
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-02-17 20:53:43 +0600

    validation part is complete

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 function getMonthValue(monthInput){
     const monthlyIncome=document.getElementById(monthInput);
     const newMonthlyIncome=parseFloat(monthlyIncome.value);
+    if(isNaN(newMonthlyIncome) || (newMonthlyIncome < 0)){
+       return alert('Please enter your monthly income balance')
+    }
     // monthlyIncome.value='';
     return newMonthlyIncome;
 }
@@ -12,7 +15,6 @@ function getMonthValue(monthInput){
 function getInput(input1,input2,input3){
     // debugger;
     const foodInput=document.getElementById(input1);
-    
     console.log(typeof isNaN(foodInput.value))
     const newFoodInput=parseFloat(foodInput.value);
     console.log(typeof newFoodInput)
@@ -20,50 +22,82 @@ function getInput(input1,input2,input3){
     const newRentInput=parseFloat(rentInput.value);
     const clothInput=document.getElementById(input3);
     const newClothInput=parseFloat(clothInput.value);
-    foodInput.value=''
-    rentInput.value=''
-    clothInput.value=''
+    
     // validate all input value 
     console.log(typeof foodInput.value);
     if(isNaN(newFoodInput) && isNaN(newRentInput) && isNaN(newClothInput)){
         return alert('Please fill up the expense form');
     }
-    if(isNaN(newFoodInput)){
-        document.querySelectorAll('.food').setAttribute('id','food-validation');
-        
-    }
-    if(isNaN(newRentInput)){
-        document.querySelectorAll('.food')[1].setAttribute('id','rent-validation');
-       
-    }
-    if(isNaN(newClothInput)){
-        document.querySelectorAll('.food')[2].setAttribute('id','cloth-validation');
-        
+    if(isNaN(newFoodInput) || (newFoodInput < 0)){
+      return document.querySelectorAll('.food')[0].setAttribute('id','food-validation');
     }
     else{
-        const total=(newFoodInput + newRentInput + newClothInput)
-        return total;
+       document.querySelectorAll('.food')[0].removeAttribute('id');
+       
     }
-        
+    if(isNaN(newRentInput) || (newRentInput < 0)){
+      return  document.querySelectorAll('.food')[1].setAttribute('id','rent-validation');
+    }
+    else{
+        document.querySelectorAll('.food')[1].removeAttribute('id');
+    }
+    if(isNaN(newClothInput) && (newClothInput < 0)){
+      return  document.querySelectorAll('.food')[2].setAttribute('id','cloth-validation');
+    }
+    else{
+        document.querySelectorAll('.food')[2].removeAttribute('id');
+    }
+    // set null all input value 
+    foodInput.value=''
+    rentInput.value=''
+    clothInput.value=''
     
+    const total=(newFoodInput + newRentInput + newClothInput)
+    return total;
     // calculate expense input value 
     
 }
 // calculate month and expense input value 
 function expensesInput(){
+    // debugger;
         const input=getInput('food-input','rent-input','cloth-input');
         console.log(typeof input);
         if(typeof input == 'undefined'){
             return false;
         }
         const monthIncome=getMonthValue('month-input');
+        if(typeof monthIncome == 'undefined'){
+            return false;
+        }
         const totalExpens=document.getElementById('total-expense');
         const totalBalance=document.getElementById('total-balance');
         // calculate total balance 
         const fullTotal=(monthIncome - input);
-        console.log(fullTotal)
+        // set extra cost here 
+        const extraCost= document.getElementById('extra-cost');
+        if(fullTotal < 0){    
+            alert('Your extra cost is' + Math.abs(fullTotal))       
+           // extraCost.setAttribute('class','extra-cost');
+          // extraCost.innerText=Math.abs(fullTotal);      
+        //    document.getElementById('extra-cost').innerText=Math.abs(fullTotal);
+        //    document.getElementById('extra-cost').style.display='block'
+        //     console.log('extra cost');
+        // console.log(extraCost.innerText)
+        }else{
+            // extraCost.innerText=00;
+            // extraCost.removeAttribute('class');
+            // extraCost.setAttribute('class','extrsa-cost2');
+            // extraCost.style.display='none';
+            
+        }
+        // console.log(fullTotal)
         totalExpens.innerText=input;
-        totalBalance.innerText=fullTotal
+        if(fullTotal < 0){
+            totalBalance.innerText=00;
+        }
+        else{
+            totalBalance.innerText=fullTotal
+        }
     
 
 }
@@ -77,15 +111,29 @@ document.getElementById('calculate').addEventListener('click',function(){
 function calculateSaving(input){
     const saving=document.getElementById(input);
     const newSaving=parseFloat(saving.value);
+    if(isNaN(newSaving)){
+       return alert('Please enter a valid input');
+    }else if(newSaving < 0){
+        return alert('Please enter a positive value');
+    }
     const montIncome=getMonthValue('month-input');
+    // console.log(typeof montIncome)
+    if(typeof montIncome == 'undefined'){
+        return false;
+    }
     const savingAmount=document.getElementById('saving-amount');
     // get total balance 
     const totalBalance=document.getElementById('total-balance'); 
     const newTotalBalance=parseFloat(totalBalance.innerText);
     // calculate totalSaving value 
-    const totalSaving=((montIncome / 100) * newSaving);
+    if(newTotalBalance == 0){
+      return document.getElementById('saving-alert').style.display='block';
+    }else{
+        document.getElementById('saving-alert').style.display='none';
+    }
+    const totalSaving=((montIncome / 100) * newSaving).toFixed(2);
     savingAmount.innerText=totalSaving;
-    document.getElementById('remaining-balance').innerText=(newTotalBalance - totalSaving)
+    document.getElementById('remaining-balance').innerText=(newTotalBalance - totalSaving).toFixed(2)
     console.log(totalSaving)
 }
 
